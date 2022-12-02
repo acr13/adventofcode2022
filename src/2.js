e857070f42be453e202c2e92fec54d99d8189564
acr13 <alex@alexcr.com>
2022-12-02 09:11:34 -0500

    day 2

---
+++ src/2.js
@@ -0,0 +1,73 @@
+import { readFileSync } from 'node:fs'
+
+const input = readFileSync('./inputs/2.txt', 'utf-8')
+  .split(/\r?\n/)
+  .map(line => line.split(' '));
+
+const SHAPE_SCORE = {
+  'A': 1, 'B': 2, 'C': 3,
+  'X': 1, 'Y': 2, 'Z': 3
+};
+
+const didIWin = (you, me) => {
+  if (you === 'A' && me === 'Y') {
+    return 1;
+  } else if (you === 'A' && me === 'Z') {
+    return -1;
+  } else if (you === 'B' && me === 'X') {
+    return -1;
+  } else if (you === 'B' && me === 'Z') {
+    return 1;
+  } else if (you === 'C' && me === 'X') {
+    return 1;
+  } else if (you === 'C' && me === 'Y') {
+    return -1;
+  }
+
+  return 0;
+};
+
+const p1 = (input) => {
+  let total = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    const [you, me] = input[i];
+    const whoWon = didIWin(you, me);
+    total += SHAPE_SCORE[me];
+  
+    if (whoWon === 1) {
+      total += 6;
+    } else if (whoWon === 0) {
+      total += 3;
+    }
+  }
+
+  return total;
+};
+
+const p2 = (input) => {
+  const LOSING_SHAPES = { 'A': 'Z', 'B': 'X', 'C': 'Y' };
+  const WINNING_SHAPES = { 'A': 'Y', 'B': 'Z', 'C': 'X' };
+
+  let total = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    const [you, outcome] = input[i];
+
+    if (outcome === 'X') {
+      total += SHAPE_SCORE[LOSING_SHAPES[you]];
+    } else if (outcome === 'Y') {
+      total += 3 + SHAPE_SCORE[you];
+    } else {
+      total += 6 + SHAPE_SCORE[WINNING_SHAPES[you]];
+    }
+  }
+
+  return total;
+};
+
+
+
+
+console.log('Part one:', p1(input));
+console.log('Part two:', p2(input));
